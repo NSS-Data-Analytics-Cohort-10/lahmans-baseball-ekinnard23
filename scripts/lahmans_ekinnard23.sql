a503162ce6cd7f57c6ecba75afcebcc646438929
ekinnard23 <ekinnard@gmail.com>
2023-10-28 10:44:04 -0500

    8

--- scripts/lahmans_ekinnard23.sql
+++ scripts/lahmans_ekinnard23.sql
@@ -148,8 +148,63 @@ ORDER BY w ASC
 7.c Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
 
+
+WITH league_win_rank AS (SELECT name,
+						w,
+						wswin,
+						Rank() OVER (PARTITION BY yearid ORDER BY w DESC) AS 							total_league_win_rank
+						FROM teams
+						WHERE yearid <>1981
+						AND yearid >= 1970
+						ORDER BY total_league_win_rank)
+SELECT COUNT(*) AS wins_for_team_w_most_wins,
+	ROUND((COUNT(*)*1.0/46) * 100, 2) AS wins_for_team_w_most_wins_percentage
+FROM league_win_rank
+WHERE wswin = 'Y'
+	AND total_league_win_rank = 1
+
+--12 teams - 26.09%
+
 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
+WITH top_2016_attendance AS (SELECT park_name,
+								team,
+								homegames.attendance/games AS avg_attendance
+						FROM homegames
+						INNER JOIN parks
+						USING(park)
+						WHERE games >=10 AND year = 2016
+						ORDER BY avg_attendance DESC
+						LIMIT 5)
+							
+SELECT park_name,
+	teams.name,
+	avg_attendance
+	FROM top_2016_attendance
+	JOIN teams
+	ON teams.teamid = top_2016_attendance.team
+	WHERE yearid =2016
+
+WITH bottom_2016_attendance AS (SELECT park_name,
+								team,
+								homegames.attendance/games AS avg_attendance
+						FROM homegames
+						INNER JOIN parks
+						USING(park)
+						WHERE games >=10 AND year = 2016
+						ORDER BY avg_attendance ASC
+						LIMIT 5)
+							
+SELECT park_name,
+	teams.name,
+	avg_attendance
+	FROM bottom_2016_attendance
+	JOIN teams
+	ON teams.teamid = bottom_2016_attendance.team
+	WHERE yearid =2016
+	
+--TOP: Dodgers/Dodger Stadium, Cartinals/Busch Stadium, Blue Jays/Rogers Centre, Giants/AT&T Park, Cubs/Wrigley Field
+--BOTTOM: Rays/Tropicana Field, Athletics/Alameda Coliseum, Indians/Progressive Field, Marlins/Marlins Park, White Sox/US Cellular
 
 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
 
